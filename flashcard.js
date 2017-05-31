ca6f54de0581d8b1d6e1db9353037173d1653672
Ayas87 <randylam87@gmail.com>
2017-05-31 16:30:46 -0700

    basic card now pushes to a json file, need to work on cloze

--- flashcard.js
+++ flashcard.js
@@ -2,24 +2,50 @@ const BasicCard = require('./BasicCard.js');
 const ClozeCard = require('./ClozeCard.js');
 const express = require('express');
 const fs = require('fs');
-const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
-
-
-
-
-const firstPresident = new BasicCard('Who was the first president of the United States?','George Washington');
-const firstPresidentCloze = new ClozeCard('George Washington was the first president of the United States.', 'George Washington');
-
+const app = express();
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 // Define the port to run on
 app.set('port', 3000);
 app.use(express.static(path.join(__dirname, 'public')));
-
 // Listen for requests
 const server = app.listen(app.get('port'), function() {
   const port = server.address().port;
 });
 
-app.get('/data',function(req, res) {
-    res.json(firstPresident);
+function appendBasic (param) {
+    const options = {
+        encoding: 'utf8',
+    };
+    fs.appendFileSync('basic.json',JSON.stringify(param) + '\n',[options]);
+    console.log(param);
+}
+
+function appendCloze (param) {
+    const options = {
+        encoding: 'utf8',
+    };
+    fs.appendFileSync('cloze.json',JSON.stringify(param) + '\n',[options]);
+    console.log(param);
+}
+
+//Basic Card
+app.post('/', urlencodedParser, function (req, res) {
+  if (!req.body){
+     return res.sendStatus(400);   
+  }
+  const basicCard = new BasicCard(req.body.front, req.body.back);
+  appendBasic(basicCard);
+  res.send(basicCard);
+});
+//Cloze Card
+app.post('/', urlencodedParser, function (req, res) {
+  if (!req.body){
+     return res.sendStatus(400);   
+  }
+  const clozeCard = new ClozeCard(req.body.front, req.body.back);
+  appendCloze(clozeCard);
+  res.send(clozeCard);
 });
--- public/app.js
+++ public/app.js
@@ -1,10 +1,31 @@
-$('.btn-sample').on('click',function(){
-    $.get("http://localhost:3000/data/", function(data, status){
-        // alert("Data: " + data + "\nStatus: " + status);
-        $('.sample').append(data.front);
-        // $('.sample').append(data.back);
-        // $('.sample').append(JSON.parse(data));
-        // console.log(data);
-    });
+$('.sample').on('click', function(data){
+    if($('.sample').attr('side') === 'front'){
+            $('.sample').html(data.back);
+            $('.sample').attr('side','back');
+    } else {
+            $('.sample').html(data.front);
+            $('.sample').attr('side','front');
+    }
 });
-// 
+
+//Basic Form Submit
+$("#basicForm").submit(function(e) {
+    e.preventDefault(); // Prevents the page from refreshing
+    var $this = $(this); // `this` refers to the current form element
+    $.post($this.attr("action"), $this.serialize(), (data) => {
+            $('.sample').html(data.front);
+            $('.sample').attr('side','front');
+        },"json"
+    );
+});
+
+//Cloze Form Submit
+$("#clozeForm").submit(function(e) {
+    e.preventDefault(); // Prevents the page from refreshing
+    var $this = $(this); // `this` refers to the current form element
+    $.post($this.attr("action"), $this.serialize(),(data) => {
+            $('.sample').html(data.text);
+            $('.sample').attr('side','text');
+        },"json"
+    );
+});
